e059d79c95cf56d527d84a56a5f2c79e31dfe749
Vincent-Young4869 <vincent.yyc98@gmail.com>
2024-10-12 20:47:43 -0400

    clean up content in api module

--- ignite-kubernetes-operator/src/main/java/org/example/igniteoperator/utils/Constants.java
+++ ignite-kubernetes-operator/src/main/java/org/example/igniteoperator/utils/Constants.java
@@ -6,7 +6,7 @@ public interface Constants {
 
     String OPERATOR_NAME = "ignite-operator";
     String DEFAULT_GRIDGAIN_IMAGE = "gridgain/community";
-    String DEFAULT_FRONTEND_IMAGE = "dependencytrack/frontend";
     
     Duration RECONCILE_MAX_RETRY_DURATION = Duration.ofMinutes(2);
+    
 }
